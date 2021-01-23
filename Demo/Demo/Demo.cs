b98792d6363be46a81b047faaef514e0303529a9
Charles Holdren <CFHoldren@student.fullsail.edu>
2021-01-22 19:24:10 -0800

    Menu functionality for options 9 and 10

--- Demo/Demo/Demo.cs
+++ Demo/Demo/Demo.cs
@@ -91,10 +91,10 @@ namespace Demo
                     Footer(_uiOptions[2]);
                     return true;
                 case 9:
-                    ChangeForeground();
+                    ChangeForeground(_uiOptions[3]);
                     return true;
                 case 10:
-                    ChangeBackground();
+                    ChangeBackground(_uiOptions[4]);
                     return true;
             }
 
@@ -497,14 +497,124 @@ namespace Demo
             Continue();
         }
 
-        private static void ChangeForeground()
+        private static void ChangeForeground(string title)
         {
+            Console.Clear();
 
+            UI.Header(title, ConsoleColor.Green);
+
+            UI.ChangeForeground("\r\nThis demo option will demonstrate a formatting" +
+                " method that changes the Foreground color of the passed text.\r\n\r\n\r\n", ConsoleColor.Gray);
+
+            UI.Footer("Please enter a message: ");
+
+            string response = Console.ReadLine();
+
+            response = Validation.ForString(response);
+
+            Console.Clear();
+
+            UI.Header(title, ConsoleColor.Green);
+
+            List<string> colors = new List<string>()
+            {
+                "Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray",
+                "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray",
+                "Green", "Magenta", "Red", "White", "Yellow"
+            };
+
+            for (int i = 0; i < colors.Count; i++)
+            {
+                if (i == 0)
+                {
+                    UI.ChangeForeground($"[{i + 1}] {colors[i]}", ConsoleColor.Gray);
+                }
+                else
+                {
+                    UI.ChangeForeground($"\r\n[{i + 1}] {colors[i]}", ConsoleColor.Gray);
+                }
+            }
+
+            Console.WriteLine("\r\n\r\n");
+            UI.Footer("Select a color by it's number option: ");
+
+            string response2 = Console.ReadLine();
+
+            int num = Validation.IntRange("color", 1, 16, response2);
+
+            ConsoleColor c = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colors[num - 1]);
+
+            Console.Clear();
+
+            UI.Header("Preview", ConsoleColor.Green);
+
+            Console.WriteLine("\r\n");
+
+            UI.ChangeForeground(response, c);
+
+            Console.WriteLine("\r\n");
+
+            Continue();
         }
 
-        private static void ChangeBackground()
+        private static void ChangeBackground(string title)
         {
+            Console.Clear();
 
+            UI.Header(title, ConsoleColor.Green);
+
+            UI.ChangeForeground("\r\nThis demo option will demonstrate a formatting" +
+                " method that changes the Foreground color of the passed text.\r\n\r\n\r\n", ConsoleColor.Gray);
+
+            UI.Footer("Please enter a message: ");
+
+            string response = Console.ReadLine();
+
+            response = Validation.ForString(response);
+
+            Console.Clear();
+
+            UI.Header(title, ConsoleColor.Green);
+
+            List<string> colors = new List<string>()
+            {
+                "Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray",
+                "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray",
+                "Green", "Magenta", "Red", "White", "Yellow"
+            };
+
+            for (int i = 0; i < colors.Count; i++)
+            {
+                if (i == 0)
+                {
+                    UI.ChangeForeground($"[{i + 1}] {colors[i]}", ConsoleColor.Gray);
+                }
+                else
+                {
+                    UI.ChangeForeground($"\r\n[{i + 1}] {colors[i]}", ConsoleColor.Gray);
+                }
+            }
+
+            Console.WriteLine("\r\n\r\n");
+            UI.Footer("Select a color by it's number option: ");
+
+            string response2 = Console.ReadLine();
+
+            int num = Validation.IntRange("color", 1, 16, response2);
+
+            ConsoleColor c = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colors[num - 1]);
+
+            Console.Clear();
+
+            UI.Header("Preview", ConsoleColor.Green);
+
+            Console.WriteLine("\r\n");
+
+            UI.ChangeBackground(response, c);
+
+            Console.WriteLine("\r\n");
+
+            Continue();
         }
 
         private static void Continue()
